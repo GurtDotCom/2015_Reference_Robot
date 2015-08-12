0c15b07cb5caf935f8edcbf46a4e35b631e7b788
Garrett <gurt1@gurt.com>
2015-08-11 23:02:58 -0700

    Update FrameProcessor.cs

--- sw/TowerDefenderMkII/FrameProcessor.cs
+++ sw/TowerDefenderMkII/FrameProcessor.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -13,16 +13,18 @@ namespace TowerDefender
 {
     public class FrameProcessor
     {
-        private RGB _color = new RGB(255, 255, 255);
+        //private RGB _color = new RGB(255, 255, 255);
+        private RGB _color = new RGB(GlobalVar.Red, GlobalVar.Blue, GlobalVar.Green);
         private int thresh_val = 120;
         private AForge.Point center = new AForge.Point(240, 160);
         private LaserController _controller;
         private BlobCounter bc;
+        private bool alreadyZeroized = false;
 
         public FrameProcessor(LaserController controller)
         {
             _controller = controller;
-            var min_size_val = 4;
+            var min_size_val = 8;
 
             bc = new BlobCounter();
             bc.FilterBlobs = true;
@@ -35,6 +37,7 @@ namespace TowerDefender
         public Bitmap ProcessFrame(Bitmap frame)
         {
             var filter = new EuclideanColorFiltering();
+            _color = new RGB(GlobalVar.Red, GlobalVar.Blue, GlobalVar.Green);
             filter.CenterColor = _color;
             filter.Radius = 20;
             filter.ApplyInPlace(frame);
@@ -65,9 +68,36 @@ namespace TowerDefender
                 }
 
                 var closestDistance = center.SquaredDistanceTo(closest);
-                var shouldFire = closestDistance < 20;
+                var shouldFire = closestDistance < 100; //start firing when close (defualt=20)
                 var delta = closest - center;
-                _controller.Update(delta.X, delta.Y, shouldFire);
+             //   _controller.Update(delta.X, delta.Y, shouldFire);
+
+                GlobalVar.objectX = closest.X;
+                GlobalVar.objectY = closest.Y;
+                //send command to move
+
+                if (GlobalVar.calibrationMode)
+                {
+                    if (GlobalVar.calibrationMoveComplete)
+                    {
+
+                        GlobalVar.xRatio = delta.X / 100;
+                        //GlobalVar.xRatio = delta.X;
+
+                        if (GlobalVar.xRatio < 0) { GlobalVar.xRatio *= -1; }
+                    }
+                    else
+                    {
+                        _controller.Calibrate();
+                        GlobalVar.calibrationMoveComplete = true;
+                    }
+                }
+                else
+                {
+                    _controller.Update(delta.X, delta.Y, shouldFire);
+                    alreadyZeroized = false;
+                }
+
 
                 var g = Graphics.FromImage(frame);
                 using (Pen p = new Pen(Color.Red))
@@ -82,6 +112,56 @@ namespace TowerDefender
                 {
                     g.DrawRectangle(p, closest.X - 2, closest.Y - 2, 5, 5);
                 }
+
+                GlobalVar.nothingTime = 0;
+            }
+            else
+            {
+                //     _controller.Hunt();
+                GlobalVar.nothingTimerEnabled = true;
+                if (GlobalVar.nothingTime == 150)
+                {//10 = 1 second
+                    if (!alreadyZeroized)
+                    {
+                        _controller.FastZeroize();
+                        alreadyZeroized = true;
+                    }
+                }
+                if (GlobalVar.nothingTime == 300)
+                {//10 = 1 second
+                    //GlobalVar.nothingTime = 0;
+
+                    _controller.SlowZeroize();
+                    alreadyZeroized = true;
+                    GlobalVar.nothingTime = 0;
+
+                }
+                /*
+                if (GlobalVar.nothingTime == 45)
+                {//10 = 1 second
+                    _controller.Hunt();
+                    alreadyZeroized = false;
+                }
+                if (GlobalVar.nothingTime == 55)
+                    _controller.Hunt();
+
+                if (GlobalVar.nothingTime == 65)
+                    _controller.Hunt();
+
+                if (GlobalVar.nothingTime == 75)
+                    _controller.Hunt();
+                    
+                if (GlobalVar.nothingTime == 85)
+                    _controller.Hunt();
+                
+                if (GlobalVar.nothingTime == 90)
+                  {
+                    GlobalVar.nothingTime = 0;
+                     _controller.Hunt();
+                  }
+                  
+                 */
+
             }
 
             return frame;
@@ -96,5 +176,16 @@ namespace TowerDefender
         {
             center = new AForge.Point(p1, p2);
         }
+
+        public static void ColorToHSV(Color color, out double hue, out double saturation, out double value)
+        {
+            int max = Math.Max(color.R, Math.Max(color.G, color.B));
+            int min = Math.Min(color.R, Math.Min(color.G, color.B));
+
+            hue = color.GetHue();
+            saturation = (max == 0) ? 0 : 1d - (1d * min / max);
+            value = max / 255d;
+        }
+
     }
 }
